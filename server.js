77683c934383179a1df415e27a7534c22f0f0e73
alehandro-777 <alexande.rekalo@gmail.com>
2022-11-14 20:01:10 +0200

    add prod server

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require('express')
 const port = process.env.PORT || 3002;
 const app = express();
 
-app.use(express.static('./dist/e-commer-angular9'));
+app.use(express.static('./dist/oask-info-portal-angular-bootstrap'));
 
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/e-commer-angular9/'});
+    res.sendFile('index.html', {root: 'dist/oask-info-portal-angular-bootstrap/'});
 });
 
 console.log(`Server started at port: ${port}`)
